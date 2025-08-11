1ef1a4e38e2456ad5e9d5e005ce6a6d22f04cafe
Elionai Moura Cordeiro <eli.embits@gmail.com>
2025-08-10 22:20:24 -0300

    Created angular project basis and styling

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, Injector, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export let AppInjector: Injector;
+
+export function setAppInjector(injector: Injector) {
+  AppInjector = injector;
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, Injector } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { setAppInjector } from './app.config';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tasksclient');
+  isSmallScreen = false;
+  
+  constructor(
+    private injector: Injector
+  ) {
+    inject(BreakpointObserver)
+    .observe([ Breakpoints.XSmall, Breakpoints.Small ])
+    .pipe(takeUntilDestroyed())
+    .subscribe(result => {
+      this.isSmallScreen = result.matches;
+    });
+    setAppInjector(this.injector);
+  }
+
+  createTask(): void {
+    // TODO
+  }
+
 }
